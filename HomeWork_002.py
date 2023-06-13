75927043ce3f078d37488320b7f7ca8ce6369b24
Krimsor <v_suffering@mail.ru>
2023-06-13 11:13:08 +1000

    Create HW_002

--- HomeWork_002.py
+++ HomeWork_002.py
@@ -1,5 +1,7 @@
+# Реализовать алгоритм пирамидальной сортировки (сортировка кучей).
+
 def heapify(arr, n, i):
-    largest = i # Initialize largest as root
+    largest = i  # Initialize largest as root
     l = 2 * i + 1   # left = 2i + 1
     r = 2 * i + 2   # right = 2*i + 2
 
@@ -15,12 +17,14 @@ def heapify(arr, n, i):
 
     # Заменяем корень, если нужно
     if largest != i:
-        arr[i],arr[largest] = arr[largest],arr[i] # свап
+        arr[i], arr[largest] = arr[largest], arr[i]  # свап
 
         # Применяем heapify к корню.
         heapify(arr, n, largest)
 
 # Основная функция для сортировки массива заданного размера
+
+
 def heapSort(arr):
     n = len(arr)
 
@@ -30,13 +34,14 @@ def heapSort(arr):
 
     # Один за другим извлекаем элементы
     for i in range(n-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i] # свап 
+        arr[i], arr[0] = arr[0], arr[i]  # свап
         heapify(arr, i, 0)
+
 
 # Управляющий код для тестирования
 arr = [12, 11, 13, 5, 6, 7]
 heapSort(arr)
 n = len(arr)
-print ("Sorted array is")
+print("Sorted array is")
 for i in range(n):
-    print ("%d" %arr[i])
+    print("%d" % arr[i])
